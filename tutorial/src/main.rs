5ebc118c6100bc4adba4196982658ae2854cb4f8
Joseph Sepich <joe@sepich.dev>
2023-08-15 14:09:44 -0400

    sprites

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -22,6 +22,11 @@ fn main() {
     let mut game = Game::new();
 
     // game setup
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation = Vec2::new(0.0, 0.0);
+    player.rotation = SOUTH_WEST;
+    player.scale = 1.0;
+    player.layer = 1.0;
 
     // can add multiple game logic functions, ran in order added
     game.add_logic(game_logic);
@@ -30,6 +35,6 @@ fn main() {
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     // game logic
-    println!("Current Score: {}", game_state.current_score);
+    // println!("Current Score: {}", game_state.current_score);
     game_state.current_score += 1;
 }
